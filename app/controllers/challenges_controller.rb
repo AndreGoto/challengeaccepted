cad53add5ad31789673035c40cf3ded8b8e309d1
Fillype Farias <fillype1@hotmail.com>
2016-12-06 14:36:33 -0200

    add banner

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,13 @@
 class ChallengesController < ApplicationController
+
+
+  def show
+    @challange = Challange.find[params:id]
+  end
+
+
+  def challange_params
+    params.require(:job).permit(:title, :description, :rule, :picture, :start_date, :end_date)
+  end
+
 end
