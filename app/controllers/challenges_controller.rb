44438370baebabaaf0dc3a7d253b9d31e36f2ad8
Sonia Arai <soninhanaomi@gmail.com>
2016-12-06 17:33:42 -0200

    add carrierwave to form

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,11 +1,21 @@
 class ChallengesController < ApplicationController
 
   def new
-    @challenges = Challenge.new
+    @challenge = Challenge.new
   end
 
   def create
+    @challenge = Challenge.new(challenge_params)
+    @challenge.owner = current_user
+    if @challenge.save
+      redirect_to challenge_path(@challenge)
+    else
+      render :new
+    end
+  end
 
+  def challenge_params
+    params.require(:challenge).permit(:title, :description, :rules, :picture, :start_date, :end_date, :id_user_owner, :picture_cache)
   end
 
 end
