10f6e1f368ee86f9afcff90a66f6bfc780d35f89
Fillype Farias <fillype1@hotmail.com>
2016-12-07 16:35:03 -0200

    add interpolation to the first name on email

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,6 +1,7 @@
 class InviteMailer < ApplicationMailer
 
   def welcome(sender, email)
+    @sender = sender
     mail(to: email) do |format|
       format.html { render 'welcome.html.erb' }
     end
