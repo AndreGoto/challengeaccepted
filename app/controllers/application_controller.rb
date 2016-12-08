fbea1e1ed868aac2eee0ed53ea425e9669237954
Leticia Almeida <leticia.almeida@gmail.com>
2016-12-08 15:09:00 -0200

    Profile edit view and picture choice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+
   include Pundit
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -19,5 +22,15 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :picture, :picture_cache])
+    #  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,
+    #   :password_confirmation) }
+    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password,
+    #   :password_confirmation, :current_password, :picture, :picture_cache) }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def generate_profile_image(user)
     if user.picture?
-      cl_image_tag(user.picture.path, { class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown", crop: :thumb, gravity: :face })
+      cl_image_tag(user.picture, { class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown", crop: :thumb, gravity: :face })
     elsif user.facebook_picture_url?
       image_tag(user.facebook_picture_url, class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown")
     else
