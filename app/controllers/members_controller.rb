263e2e2a33d55891e4982f02d8320b1c4ab16b5b
Sonia Arai <soninhanaomi@gmail.com>
2016-12-09 16:03:22 -0200

    apply member - incomplete: still need to check and match invitation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,14 @@
 class MembersController < ApplicationController
+  def create
+    @member = Member.new
+    @member.user = current_user
+    @member.challenge_id = params[:challenge_id].to_i
+    authorize @member
+    if @member.save
+      flash[:notice] = "You're new member"
+      redirect_to challenge_path(challenge)
+    else
+      redirect_to challenge_path(challenge)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   resources :challenges do
+    resources :members, only: [:create]
     get 'invite', on: :member
     post 'send_invite', on: :member
     resources :challenge_messages, only: :create
