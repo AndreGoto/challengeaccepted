808a2b3dfcb782f2d2847ea8871742d0872ad332
Leticia Almeida <leticia.almeida@gmail.com>
2016-12-13 14:48:23 -0200

    dashboard adjusting

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,19 +3,22 @@ class DashboardController < ApplicationController
     @challenges = policy_scope(Challenge)
 
     challenges = current_user.challenges
+
     @challenges_progress = []
     @challenges_ended = []
-    @challenge_created = Challenge.where(user_id: current_user.id)
 
-    # @challenge_waiting = Challenge.invite
+    @challenge_created = Challenge.where(user_id: current_user.id)
+    @waiting_approval = ChallengeRequest.where(user_id: current_user.id)
 
     challenges.each do |challenge|
       if challenge.end_date < Date.today
          @challenges_ended << challenge
-      else
+      elsif
         @challenges_progress << challenge
       end
     end
   end
 
 end
+
+
